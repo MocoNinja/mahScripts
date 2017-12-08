ab63b333ef3872d6e296e876c2733bd8ec53ff81
MocoNinja <javiergonzaleztello@gmail.com>
2017-12-08 02:54:31 +0100

    Metido un poco de morraya en el script para que no lo ejecute 5 veces como un gilipollas
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- Fedora/fontRendering.sh
+++ Fedora/fontRendering.sh
@@ -1,5 +1,9 @@
+echo "Instalando renderizado de fuentes..."
 sudo dnf install freetype-freeworld
 if [ -f /etc/fonts/local.conf ]; then
+echo "Borrando archivo existente de configuración..."
 sudo rm /etc/fonts/local.conf
 fi
+echo "Instalando archivo optimizado de configuración..."
 sudo cp local.conf /etc/fonts
+echo "Instalación exitosa!"
