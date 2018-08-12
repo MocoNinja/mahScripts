63e73c1784d07a18d18b1e27e732f55995c7f10c
MocoNinja <javiergonzaleztello@gmail.com>
2018-08-12 20:53:46 +0200

    Poniendo en evidencia que soy retrasado desde que me parió mi madre
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- MahBootstrap/install_raw.sh
+++ MahBootstrap/install_raw.sh
@@ -5,24 +5,25 @@ FILENAME="packages_raw"
 MASTER_LINE=""
 
 function appendToMasterLine() {
-	# echo "The line to append is $1"
-	MASTER_LINE="$MASTER_LINE"" ""$1"
+    #echo "The line to append is $1"
+    MASTER_LINE="$MASTER_LINE"" ""$1"
 }
 
 function readLine() {
-	while IFS='' read -r line || [[ -n "$line" ]]; do
-		if [[ $line =~ ^# ]]; then 
-			echo "That was a comment" >> /dev/null
-		else
-			# echo "Text read from file: $line"
-			appendToMasterLine $line
-		fi
-	done < "$1"
+    while IFS=' ' read -r line || [[ -n "$line" ]]; do
+        if [[ $line =~ ^# ]]; then 
+            echo "That was a comment" >> /dev/null
+        else
+            # echo "Text read from file: $line"
+            appendToMasterLine "$line"
+        fi
+    # done < "$FILENAME"
+    done < "$1"
 }
 
 readLine $FILENAME
 
-echo "AFTER PARSING, THE \$MASTER_LINE IS:$MASTER_LINE" >> /dev/null
+# echo "AFTER PARSING, THE \$MASTER_LINE IS:$MASTER_LINE" # >> /dev/null
 
 sudo apt install $MASTER_LINE
 
