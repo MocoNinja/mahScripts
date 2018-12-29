5da23e6e8cbd6a7db015e48e1b07bce00fc185ab
MocoNinja <javiergonzaleztello@gmail.com>
2018-12-29 19:17:17 +0100

    Working on font rendering...
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- Fedora/fontRendering.sh
+++ Fedora/fontRendering.sh
@@ -1,9 +1,25 @@
 echo "Instalando renderizado de fuentes..."
 sudo dnf install -y freetype-freeworld
+
 if [ -f /etc/fonts/local.conf ]; then
-echo "Borrando archivo existente de configuración..."
-sudo rm /etc/fonts/local.conf
+echo "Respaldando archivo existente de configuración..."
+sudo cp /etc/fonts/local.conf /etc/fonts/local.conf.OLD
 fi
+
 echo "Instalando archivo optimizado de configuración..."
 sudo cp local.conf /etc/fonts
+echo "Instalación exitosa!"
+
+echo "Instalando configuración en xResources..."
+cat << until_it_ends | sudo tee /home/$USER/.Xresources > /dev/null
+Xft.dpi: 96
+Xft.antialias: true
+Xft.hinting: true
+Xft.rgba: rgb
+Xft.autohint: false
+Xft.hintstyle: hintslight
+Xft.lcdfilter: lcddefault
+until_it_ends
+
+xrdb -merge /home/$USER/.Xresources
 echo "Instalación exitosa!"
